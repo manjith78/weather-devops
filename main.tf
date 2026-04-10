2abec082b4e268fbb3e40a7c36f75de22704c524
manjith78 <manjithmanjith78@gmail.com>
2026-04-11 00:50:35 +0530

    Initial

--- main.tf
+++ main.tf
@@ -7,46 +7,16 @@ resource "azurerm_resource_group" "rg" {
   location = "East US"
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "weather-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "weather-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_network_interface" "nic" {
-  name                = "weather-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "weather-vm"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = "Standard_B1s"
   admin_username      = "azureuser"
-
-  network_interface_ids = [
-    azurerm_network_interface.nic.id
-  ]
-
-  admin_password = "Password@1234"
-
+  admin_password      = "Password@1234"
   disable_password_authentication = false
+
+  network_interface_ids = []
 
   os_disk {
     caching              = "ReadWrite"
